d275ad1fa0b7995c4cd8bd30f14deaece6bd2baa
Lucia <luciaberonwaingart@hotmail.com>
2021-08-25 20:59:31 -0300

    project images replaced

--- src/content/projects.js
+++ src/content/projects.js
@@ -4,7 +4,7 @@ export const projects = [
       description: 'Página de administración de finanzas personales, que permite al usuario registrarse y manejar sus gastos.',
       languages: ['html','css','javascript','react','bootstrap','nodejs','mysql','ex'],
       link:"https://github.com/mindlessChair/Administrador-de-finanzas-",
-      img:'https://images.pexels.com/photos/2061168/pexels-photo-2061168.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      img:'https://i2.lensdump.com/i/ZUrAck.png',
     },
     {
       name: 'Portafolio',
@@ -12,7 +12,7 @@ export const projects = [
         'Portafolio personal.',
       languages: ['html','css','javascript','react','bootstrap','sass'],
       link:"https://github.com/mindlessChair/portfolio",
-      img:'https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      img:'https://i1.lensdump.com/i/ZFMFyH.png',
     },
     {
       name: 'Administrador de publicaciones',
@@ -35,7 +35,7 @@ export const projects = [
         'Proyecto dado por The Odin Project. Consiste en una biblioteca personal en la que pueden agregarse, quitarse y modicarse libros que fueron leídos por el usuario. Los libros se guardan por medio de cookies.',
       languages: ['html','css','javascript'],
       link:"https://github.com/mindlessChair/Library",
-      img:'https://images.pexels.com/photos/2004161/pexels-photo-2004161.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      img:'https://i3.lensdump.com/i/ZUriZx.png',
     },
     {
       name: 'Cliente de tiempo y clima',
@@ -43,6 +43,6 @@ export const projects = [
         'Cliente que manipula API de tiempo y clima, implementando un buscador que permite ingresar una localidad para la cual mostrar tiempo y clima.',
       languages: ['html','css','react','typescript', 'bootstrap'],
       link:"https://github.com/mindlessChair/client-test",
-      img:'https://images.pexels.com/photos/943096/pexels-photo-943096.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      img:'https://i.lensdump.com/i/ZUrjsH.png',
     },
   ];
